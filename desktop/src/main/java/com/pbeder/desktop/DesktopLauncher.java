8e4ea0caf00b58177c971c16afb63afaea2c008f
Paweł Beder <pawel.beder@gmail.com>
2017-10-22 17:31:25 +0200

    FileChoser in DesktopLauncher to easier switch between roms.

--- desktop/src/main/java/com/pbeder/desktop/DesktopLauncher.java
+++ desktop/src/main/java/com/pbeder/desktop/DesktopLauncher.java
@@ -3,18 +3,30 @@ package com.pbeder.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.pbeder.Application;
-import org.lwjgl.Sys;
+
+import java.awt.*;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+    public static void main(String[] arg) {
+        String file = selectRom();
+        configureLibGdx(file);
+    }
+
+    private static void configureLibGdx(String file) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         config.title = "Chip8";
         config.width = Application.WORLD_WIDTH * 20;
         config.height = Application.WORLD_HEIGHT * 20;
-        if (arg.length != 1) {
-            System.out.println("Please provide file to load");
-            System.exit(-1);
-        }
-        new LwjglApplication(new Application(arg[0]), config);
-	}
+        new LwjglApplication(new Application(file), config);
+    }
+
+    private static String selectRom() {
+        FileDialog dialog = new FileDialog((Frame)null, "Select File to Open");
+        dialog.setMode(FileDialog.LOAD);
+        dialog.setVisible(true);
+        String file = dialog.getDirectory() + dialog.getFile();
+        System.out.println(file + " chosen.");
+        dialog.dispose();
+        return file;
+    }
 }
